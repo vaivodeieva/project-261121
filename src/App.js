b52c73c6396f802be936ef324e0612ef72b931c4
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2021-11-27 14:34:26 +0200

    add header, footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Views/Home';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
 
 
 function App() {
