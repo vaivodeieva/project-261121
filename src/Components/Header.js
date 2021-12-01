c1434b8fdfa40b58c58b97f47e29b87aa1ac0c42
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2021-12-01 09:54:31 +0200

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -7,7 +7,7 @@ function Header() {
         var sc = $(window).scrollTop()
         if (sc > 150) {
             $("#main-navbar").addClass("navbar-scroll")
-        } 
+        }
         else {
             $("#main-navbar").removeClass("navbar-scroll")
         }
@@ -17,39 +17,39 @@ function Header() {
 
         <div>
             <header>
-	<nav id="main-navbar" className="navbar navbar-default navbar-fixed-top">
-		<div className="container navbar-container">
-			<div className="navbar-header">
-				<button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-					<span className="sr-only">Toggle navigation</span>
-					<span className="icon-bar"></span>
-					<span className="icon-bar"></span>
-					<span className="icon-bar"></span>
-				</button>
-				<a className="navbar-brand" href="#"></a>
-			</div>
-			<div id="navbar" className="navbar-collapse collapse">
-				<ul className="nav navbar-nav navbar-right">
-					<li><a href="#">Random</a></li>
-					<li><a href="#about">Happiness</a></li>
-					<li><a href="#portfolio">Hacking</a></li>
-					<li><a href="#contact">News</a></li>
-					<li><a href="#blog">Technology</a></li>
-				</ul>
-			</div>
-			<div className="top-social">
-				<ul id="top-social-menu">
-					<li><a href="#"><i className="fa fa-twitter"></i></a></li>
-					<li><a href="#"><i className="fa fa-facebook"></i></a></li>
-					<li><a href="#">About</a></li>
-					<li><a href="#">Investors</a></li>
-				</ul>
-			</div>
-		</div>
-	</nav>
-</header>
+                <nav id="main-navbar" className="navbar navbar-default navbar-fixed-top">
+                    <div className="container navbar-container">
+                        <div className="navbar-header">
+                            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                                <span className="sr-only">Toggle navigation</span>
+                                <span className="icon-bar"></span>
+                                <span className="icon-bar"></span>
+                                <span className="icon-bar"></span>
+                            </button>
+                            <a className="navbar-brand" href="#"></a>
+                        </div>
+                        <div id="navbar" className="navbar-collapse collapse">
+                            <ul className="nav navbar-nav navbar-right">
+                                <li><a href="#">Random</a></li>
+                                <li><a href="#about">Happiness</a></li>
+                                <li><a href="#portfolio">Hacking</a></li>
+                                <li><a href="#contact">News</a></li>
+                                <li><a href="#blog">Technology</a></li>
+                            </ul>
+                        </div>
+                        <div className="top-social">
+                            <ul id="top-social-menu">
+                                <li><a href="#"><i className="fa fa-twitter"></i></a></li>
+                                <li><a href="#"><i className="fa fa-facebook"></i></a></li>
+                                <li><a href="#">About</a></li>
+                                <li><a href="#">Investors</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            </header>
 
-{/* <section className="full-width-img">
+            {/* <section className="full-width-img">
 
 </section> */}
 
