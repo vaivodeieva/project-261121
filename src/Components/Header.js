d03cb7af80b913fc55190be5b47cd95b09c03784
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2021-12-01 09:47:05 +0200

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,8 +3,44 @@ function Header() {
     return (
 
         <div>
+            <header>
+	<nav id="main-navbar" class="navbar navbar-default navbar-fixed-top">
+		<div class="container navbar-container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#">Frrk.io</a>
+			</div>
+			<div id="navbar" class="navbar-collapse collapse">
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="#">Random</a></li>
+					<li><a href="#about">Happiness</a></li>
+					<li><a href="#portfolio">Hacking</a></li>
+					<li><a href="#contact">News</a></li>
+					<li><a href="#blog">Technology</a></li>
+				</ul>
+			</div>
+			<div class="top-social">
+				<ul id="top-social-menu">
+					<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+					<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+					<li><a href="#">About</a></li>
+					<li><a href="#">Investors</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+</header>
 
-        </div>
+{/* <section class="full-width-img">
+
+</section> */}
+
+        </div >
     )
 }
 
