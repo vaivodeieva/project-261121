a9f61b05857e2f3fdd052c14e79c366f51380787
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2021-12-03 10:54:47 +0200

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,6 +6,34 @@ function Header() {
     return (
 
         <div>
+            <nav class="nav">
+  <input type="checkbox" class="nav__cb" id="menu-cb"/>
+  <div class="nav__content">
+    <ul class="nav__items">
+      <li class="nav__item">
+        <span class="nav__item-text">
+          Home
+        </span>
+      </li>
+      <li class="nav__item">
+        <span class="nav__item-text">
+          Works
+        </span>
+      </li>
+      <li class="nav__item">
+        <span class="nav__item-text">
+          About
+        </span>
+      </li>
+      <li class="nav__item">
+        <span class="nav__item-text">
+          Contact
+        </span>
+      </li>
+    </ul>
+  </div>
+  <label class="nav__btn" for="menu-cb"></label>
+</nav>
            
         </div >
     )
