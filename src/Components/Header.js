31af3077b6b78e99635ccb4e4e89c3f1f79a42ea
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2021-12-03 10:56:49 +0200

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,35 +6,35 @@ function Header() {
     return (
 
         <div>
-            <nav class="nav">
-  <input type="checkbox" class="nav__cb" id="menu-cb"/>
-  <div class="nav__content">
-    <ul class="nav__items">
-      <li class="nav__item">
-        <span class="nav__item-text">
-          Home
-        </span>
-      </li>
-      <li class="nav__item">
-        <span class="nav__item-text">
-          Works
-        </span>
-      </li>
-      <li class="nav__item">
-        <span class="nav__item-text">
-          About
-        </span>
-      </li>
-      <li class="nav__item">
-        <span class="nav__item-text">
-          Contact
-        </span>
-      </li>
-    </ul>
-  </div>
-  <label class="nav__btn" for="menu-cb"></label>
-</nav>
-           
+            <nav className="nav">
+                <input type="checkbox" className="nav__cb" id="menu-cb" />
+                <div className="nav__content">
+                    <ul className="nav__items">
+                        <li className="nav__item">
+                            <span className="nav__item-text">
+                                Home
+                            </span>
+                        </li>
+                        <li className="nav__item">
+                            <span className="nav__item-text">
+                                Works
+                            </span>
+                        </li>
+                        <li className="nav__item">
+                            <span className="nav__item-text">
+                                About
+                            </span>
+                        </li>
+                        <li className="nav__item">
+                            <span className="nav__item-text">
+                                Contact
+                            </span>
+                        </li>
+                    </ul>
+                </div>
+                <label className="nav__btn" for="menu-cb"></label>
+            </nav>
+
         </div >
     )
 }
