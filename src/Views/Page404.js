246915c2ef0763a4a1d8ac580a68ebb8e73df100
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2021-12-04 12:24:56 +0200

    Update Page404.js

--- src/Views/Page404.js
+++ src/Views/Page404.js
@@ -2,42 +2,7 @@ import '../Assets/Css/Page404.css';
 function Page404() {
     return (
         <div>
-            <div className="error"> <span>Page not found</span><span>404</span><span>Swim Home, Friend</span>
-            </div>
-            <div className="sun"></div>
-            <div className="clouds">
-                <div></div>
-                <div></div>
-                <div></div>
-            </div>
-            <div className="birds"></div>
-            <div className="sea">
-                <div className="wave w-1"></div>
-                <div className="wave w-2"></div>
-                <div className="fish"><span></span><span></span><span></span>
-                </div>
-            </div>
-            <div className="bottom">
-                <div className="grass"><span> </span><span> </span><span> </span>
-                </div>
-                <div className="grass"><span> </span><span> </span><span> </span>
-                </div>
-                <div className="grass"><span> </span><span> </span><span> </span>
-                </div>
-                <div className="grass"><span> </span><span> </span><span> </span>
-                </div>
-                <div className="circle"><span></span><span></span><span></span><span></span><span></span>
-                </div>
-                <div className="circle"><span></span><span></span><span></span><span></span><span></span>
-                </div>
-                <div className="circle"><span></span><span></span><span></span><span></span><span></span>
-                </div>
-                <div className="circle"><span></span><span></span><span></span><span></span><span></span>
-                </div>
-                <div className="grass_tw"></div>
-                <div className="grass_tw"></div>
-                <div className="grass_tw"></div>
-            </div>
+           
         </div>
     )
 }
