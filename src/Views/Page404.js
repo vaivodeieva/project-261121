af906cd02936d62aaa87974deef8e56559d19478
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2021-12-09 23:43:32 +0200

    update page404

--- src/Views/Page404.js
+++ src/Views/Page404.js
@@ -1,8 +1,35 @@
+import '../Assets/Css/Page404.css';
 
 function Page404() {
     return (
         <div>
-           
+
+            <div class="page-wrapper">
+                <div class="circles-layer">
+                    <div class="circle -white"></div>
+                    <div class="circle -red"></div>
+                    <div class="circle -white"></div>
+                    <div class="circle -red"></div>
+                </div>
+                <div class="numbers-layer">
+                    <div class="number">4</div>
+                    <div class="number">4</div>
+                    <div class="number">4</div>
+                    <div class="number">4</div>
+                    <div class="number">4</div>
+                    <div class="number">4</div>
+                    <div class="number">4</div>
+                    <div class="number">4</div>
+                    <div class="number -with-shadow">0</div>
+                    <div class="number -with-shadow">0</div>
+                    <div class="number -with-shadow">0</div>
+                    <div class="number -with-shadow">0</div>
+                </div>
+                <div class="oops-message">
+                    <div class="test">Opps... looks like You got lost</div><a class="button" href="">Try again</a>
+                </div>
+            </div>
+
         </div>
     )
 }
