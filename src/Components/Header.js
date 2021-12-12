76e6d01b9098b568a98ff9576e9aa5db1ed18648
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2021-12-12 21:30:44 +0200

    update header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,42 +1,11 @@
 import '../Assets/Css/Header.css';
-
 function Header() {
-
 
     return (
 
-        <div className='header-container'>
-            <nav className="nav">
-                <input type="checkbox" className="nav__cb" id="menu-cb" />
-                <div className="nav__content">
-                    <ul className="nav__items">
-                        <li className="nav__item">
-                            <span className="nav__item-text">
-                                Home
-                            </span>
-                        </li>
-                        <li className="nav__item">
-                            <span className="nav__item-text">
-                                Works
-                            </span>
-                        </li>
-                        <li className="nav__item">
-                            <span className="nav__item-text">
-                                About
-                            </span>
-                        </li>
-                        <li className="nav__item">
-                            <span className="nav__item-text">
-                                Contact
-                            </span>
-                        </li>
-                    </ul>
-                </div>
-                <label className="nav__btn" for="menu-cb"></label>
-            </nav>
-
-        </div >
+        <div>
+           
+        </div>
     )
 }
-
 export default Header;
