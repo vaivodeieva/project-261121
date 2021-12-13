6118779fa0c79653cb0bb4ab86d78d8e47cad7e4
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2021-12-13 09:57:15 +0200

    update header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,8 +4,48 @@ function Header() {
     return (
 
         <div>
-           
-        </div>
-    )
+            <div class="containers">
+
+                <ul class="list">
+
+                    <li class="item-list">
+                        <a class="item-link l6" href="#">
+                            <div class="item-icon-6"><i class="fas fa-home icon"></i></div>
+                            <p class="item-text-6">Home</p>
+                        </a>
+                    </li>
+
+                    <li class="item-list">
+                        <a class="item-link l6" href="#">
+                            <div class="item-icon-6"><i class="fas fa-search icon"></i></div>
+                            <p class="item-text-6">Search</p>
+                        </a>
+                    </li>
+
+                    <li class="item-list">
+                        <a class="item-link l6" href="#">
+                            <div class="item-icon-6"><i class="fas fa-fire icon"></i></div>
+                            <p class="item-text-6">Workout</p>
+                        </a>
+                    </li>
+
+                    <li class="item-list">
+                        <a class="item-link l6" href="#">
+                            <div class="item-icon-6"><i class="fas fa-bell icon"></i></div>
+                            <p class="item-text-6">Notification</p>
+                        </a>
+                    </li>
+
+                    <li class="item-list">
+                        <a class="item-link l6" href="#">
+                            <div class="item-icon-6"><i class="fas fa-cog icon"></i></div>
+                            <p class="item-text-6">Settings</p>
+                        </a>
+                    </li>
+                    </ul>
+
+            </div>
+            </div>
+            )
 }
-export default Header;
+            export default Header;
