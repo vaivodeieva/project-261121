969cfdad74fe31b04495494f4ccaa708bdd024a2
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2021-12-15 23:30:59 +0200

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,41 +1,33 @@
 import '../Assets/Css/Header.css';
 function Header() {
-
     return (
-
         <div>
             <div className="containers">
-
                 <ul className="list">
-
                     <li className="item-list">
                         <a className="item-link l6" href="#">
                             <div className="item-icon-6"><i className="fas fa-home icon"></i></div>
                             <p className="item-text-6">Home</p>
                         </a>
                     </li>
-
                     <li className="item-list">
                         <a className="item-link l6" href="#">
                             <div className="item-icon-6"><i className="fas fa-search icon"></i></div>
                             <p className="item-text-6">Search</p>
                         </a>
                     </li>
-
                     <li className="item-list">
                         <a className="item-link l6" href="#">
                             <div className="item-icon-6"><i className="fas fa-fire icon"></i></div>
                             <p className="item-text-6">Workout</p>
                         </a>
                     </li>
-
                     <li className="item-list">
                         <a className="item-link l6" href="#">
                             <div className="item-icon-6"><i className="fas fa-bell icon"></i></div>
                             <p className="item-text-6">Notification</p>
                         </a>
                     </li>
-
                     <li className="item-list">
                         <a className="item-link l6" href="#">
                             <div className="item-icon-6"><i className="fas fa-cog icon"></i></div>
@@ -43,7 +35,6 @@ function Header() {
                         </a>
                     </li>
                     </ul>
-
             </div>
             </div>
             )
