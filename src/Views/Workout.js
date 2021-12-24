b1fb78df6247d6619f1002904ef1a59227fa2877
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2021-12-24 18:58:42 +0200

    Update Workout.js

--- src/Views/Workout.js
+++ src/Views/Workout.js
@@ -6,15 +6,15 @@ function Workout() {
             <center>
                 <h1>Workout Generator</h1>
             </center>
-            <br/>
+            <br />
             <div className="workout-container-inner">
                 <input type="text-workout" name="valueId" id="myName" placeholder="Enter Name" />
-                <input type="button" id="btn-workout" className="btn" value="Generate" /><br/>
-                    <ul id="result-workout">
-                        <h4 className='li-h4'>Are you warmed up?</h4>
-                        <img className="li-workout" src="http://gdurl.com/WfqX" alt="Crunches" />
-                        <img className="li-workout" src="http://gdurl.com/57cs" alt="Pushups" />
-                    </ul>
+                <input type="button" id="btn-workout" className="btn" value="Generate" /><br />
+                <ul id="result-workout">
+                    <h4 className='li-h4'>Are you warmed up?</h4>
+                    <img className="li-workout" src="http://gdurl.com/WfqX" alt="Crunches" />
+                    <img className="li-workout" src="http://gdurl.com/57cs" alt="Pushups" />
+                </ul>
             </div>
         </div>
     )
