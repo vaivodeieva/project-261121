760c21f33f8c39ff32e4de540d7bfbd70e20ba49
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2021-12-26 20:18:00 +0200

    cleaning code

--- src/Views/Workout.js
+++ src/Views/Workout.js
@@ -1,22 +1,10 @@
-import '../Assets/Css/Workout.css';
-
 function Workout() {
+
     return (
-        <div className='workout-container'>
-            <center>
-                <h1>Workout Generator</h1>
-            </center>
-            <br />
-            <div className="workout-container-inner">
-                <input type="text-workout" name="valueId" id="myName" placeholder="Enter Name" />
-                <input type="button" id="btn-workout" className="btn" value="Generate" /><br />
-                <ul id="result-workout">
-                    <h4 className='li-h4'>Are you warmed up?</h4>
-                    <img className="li-workout" src="http://gdurl.com/WfqX" alt="Crunches" />
-                    <img className="li-workout" src="http://gdurl.com/57cs" alt="Pushups" />
-                </ul>
-            </div>
+        <div>
+
         </div>
     )
 }
+
 export default Workout;
