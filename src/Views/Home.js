b10eaf9d4a3fd892a0f227a1c0d4614395a803d2
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2022-01-02 22:58:23 +0200

    update home

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -1,10 +1,124 @@
+import '../Assets/Css/Home.css';
+import $ from 'jquery';
+
 function Home() {
+    $("#main-section").slider({
+        type: "columns",
+        count: 1
+    });
 
     return (
 
         <div>
-          
-            
+            <div id='main-section' class='' data-title='Sports Fields'>
+                <div class='field soccer' data-title="Soccer">
+                    <div class='inner'>
+                        <div class='lines'>
+                            <div class='corners'>
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                            </div>
+                            <div class='middle-line'>
+                                <span></span>
+                            </div>
+                            <div class='left-side'>
+                                <div class='penalty-area'>
+                                    <span class='penalty-ring'></span>
+                                </div>
+
+                            </div>
+                            <div class='right-side'>
+                                <div class='penalty-area'>
+                                    <span class='penalty-ring'></span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class='field futsal' data-title="Futsal">
+                    <div class='inner'>
+                        <div class='lines'>
+                            <div class='corners'>
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                            </div>
+                            <div class='middle-line'>
+                                <span></span>
+                            </div>
+                            <div class='left-side'>
+                                <div class='penalty-area'></div>
+                                <span class='middle-dots'></span>
+                            </div>
+                            <div class='right-side'>
+                                <div class='penalty-area'> </div>
+                                <span class='middle-dots'></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class='field basketball' data-title="Basketball">
+                    <div class='inner'>
+                        <div class='lines'>
+                            <div class='middle-line'>
+                                <span></span>
+                            </div>
+                            <div class='left-side'>
+                                <div class='penalty-area'>
+                                    <div class='external-curve'></div>
+                                    <div class='internal'></div>
+                                    <div class='pentalty-ring'></div>
+                                </div>
+                            </div>
+                            <div class='right-side'>
+                                <div class='penalty-area'>
+                                    <div class='external-curve'></div>
+                                    <div class='internal'></div>
+                                    <div class='pentalty-ring'></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class='field handball' data-title="Handball">
+                    <div class='inner'>
+                        <div class='lines'>
+                            <div class='middle-line'></div>
+                            <div class='left-side'>
+                                <div class='penalty-area'>
+                                    <div class='external-curve'></div>
+                                    <div class='internal-curve'></div>
+                                </div>
+                            </div>
+                            <div class='right-side'>
+                                <div class='penalty-area'>
+                                    <div class='external-curve'></div>
+                                    <div class='internal-curve'></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class='field tennis' data-title="Tennis">
+                    <div class='inner'>
+                        <div class='lines'>
+                            <div class='middle-line'></div>
+                            <div class='left-side'>
+                            </div>
+                            <div class='right-side'>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
     )
 }
