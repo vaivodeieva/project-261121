469701d4ca59f8b2c83ca304fc2332400a87e189
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2022-01-04 17:11:59 +0200

    update header, home

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -3,11 +3,21 @@ import '../Assets/Css/Home.css';
 function Home() {
 
     return (
-
-        <div>
+        <div className='home-container'>
+            
+                <div class="container">
+                    <form action="">
+                        <h1>Boost your Performance</h1>
+                        <p>9 weeks program, I'll Teach You 9 Proven Ways to Increase Your Lift </p>
+                        <div class="email-box">
+                            <i class="fas fa-envelope"></i>
+                            <input class="tbox" type="email" name="" value="" placeholder="Enter your email"/>
+                                <button class="btn" type="button" name="button">Subscribe</button>
+                        </div>
+                    </form>
+                </div>
             
         </div>
     )
 }
-
 export default Home;
