a4c0a25de28e0f377669dc85fcac2a1203e0d9f1
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2022-01-05 23:29:41 +0200

    Update Home.js

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -4,14 +4,14 @@ function Home() {
 
     return (
         <div className='home-container'>
-                <div class="container">
+                <div className="container">
                     <form action="">
                         <h1>Boost your Performance</h1>
                         <p>9 weeks program, I'll Teach You 9 Proven Ways to Increase Your Lift </p>
-                        <div class="email-box">
-                            <i class="fas fa-envelope"></i>
-                            <input class="tbox" type="email" name="" value="" placeholder="Enter your email"/>
-                                <button class="btn" type="button" name="button">Subscribe</button>
+                        <div className="email-box">
+                            <i className="fas fa-envelope"></i>
+                            <input className="tbox" type="email" name="" value="" placeholder="Enter your email"/>
+                                <button className="btn" type="button" name="button">Subscribe</button>
                         </div>
                     </form>
                 </div>
