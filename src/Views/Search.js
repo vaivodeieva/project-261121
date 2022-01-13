8a2eaabeb73f6160424d69be4fa9d328f9555035
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2022-01-13 22:33:18 +0200

    update search

--- src/Views/Search.js
+++ src/Views/Search.js
@@ -3,18 +3,12 @@ function Search() {
 
     return (
         <div>
-            <nav>
+            <nav className='search-nav'>
                 <div class="search-container">
                     <input type="text" class="search-input" placeholder="Search" />
                     <i class="bi bi-search" id="search-btn"></i>
                 </div>
             </nav>
-
-            <main>
-                <h1>Expandable Search Bar</h1>
-            </main>
-
-
         </div>
     )
 }
