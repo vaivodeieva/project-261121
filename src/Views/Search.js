67d5672c52a6f0b424443e6d294421f2ee538076
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2022-01-14 13:55:48 +0200

    update search

--- src/Views/Search.js
+++ src/Views/Search.js
@@ -3,12 +3,46 @@ function Search() {
 
     return (
         <div>
-            <nav className='search-nav'>
-                <div class="search-container">
-                    <input type="text" class="search-input" placeholder="Search" />
-                    <i class="bi bi-search" id="search-btn"></i>
+            <div class="widget-wrap">
+                <h1>SEARCHABLE LIST</h1>
+
+                <input type="text" id="the-filter" placeholder="Search For..." autocomplete="off" />
+
+                <ul id="the-list">
+                    <li>Apple</li>
+                    <li>Apricots</li>
+                    <li>Avocado</li>
+                    <li>Banana</li>
+                    <li>Blackberries</li>
+                    <li>Blueberries</li>
+                    <li>Cherries</li>
+                    <li>Coconut</li>
+                    <li>Cranberries</li>
+                    <li>Durian</li>
+                    <li>Elderberries</li>
+                    <li>Grapefruit</li>
+                    <li>Grapes</li>
+                    <li>Guava</li>
+                    <li>Honeydew</li>
+                    <li>Jackfruit</li>
+                    <li>Longan</li>
+                    <li>Loquat</li>
+                    <li>Mango</li>
+                    <li>Orange</li>
+                    <li>Papaya</li>
+                    <li>Pear</li>
+                    <li>Pineapple</li>
+                    <li>Strawberries</li>
+                    <li>Watermelon</li>
+                </ul>
+
+                <div id="code-boxx">
+                    Visit
+                    <a href="https://code-boxx.com/filter-search-list-in-javascript/" target="_blank">
+                        Code Boxx
+                    </a> for more details.
                 </div>
-            </nav>
+            </div>
         </div>
     )
 }
