2d4b443b6d265aea1f0fee85aea1adfc680a16fc
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2022-01-28 16:06:00 +0200

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -97,7 +97,7 @@ function Footer() {
                         <div className="row">
                             <div className="col-xl-6 col-lg-6 text-center text-lg-left">
                                 <div className="copyright-text">
-                                    <p>Copyright &copy; 2021, All Rights Reserved</p>
+                                    <p>Copyright &copy; 2022 All Rights Reserved</p>
                                 </div>
                             </div>
                             <div className="col-xl-6 col-lg-6 d-none d-lg-block text-right">
