2267df130436541ce917dac5349d2c83810e2a4b
vaivodeieva <ivaivode@inbox.lv>
2022-02-03 13:01:39 +0200

    Update Home.js

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -2,17 +2,17 @@ import '../Assets/Css/Home.css';
 function Home() {
     return (
         <div className='home-container'>
-                <div className="container-inner">
-                    <form action="">
-                        <h1>Boost your Performance</h1>
-                        <p>9 weeks program, I'll Teach You 9 Proven Ways to Increase Your Lift </p>
-                        <div className="email-box-home">
-                            <span className="fas fa-envelope"></span>
-                            <input className="tbox-home" type="email" name="" placeholder="Enter your email"/>
-                            <button className="btn-home"  name="button-home" style={{fontSize: '20px'}}>Subscribe</button>
-                        </div>
-                    </form>
-                </div>
+            <div className="container-inner">
+                <form action="">
+                    <h1>Boost your Performance</h1>
+                    <p>9 weeks program, I'll Teach You 9 Proven Ways to Increase Your Lift </p>
+                    <div className="email-box-home">
+                        <span className="fas fa-envelope"></span>
+                        <input className="tbox-home" type="email" name="" placeholder="Enter your email" />
+                        <button className="btn-home" name="button-home" style={{ fontSize: '20px' }}>Subscribe</button>
+                    </div>
+                </form>
+            </div>
         </div>
     )
 }
