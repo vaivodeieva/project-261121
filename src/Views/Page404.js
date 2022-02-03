a2a60b3fdaf4e8c0e0d43e6ec41f3ea6782426b3
vaivodeieva <ivaivode@inbox.lv>
2022-02-03 12:50:48 +0200

    Update Page404.js

--- src/Views/Page404.js
+++ src/Views/Page404.js
@@ -1,9 +1,7 @@
 import '../Assets/Css/Page404.css';
-
 function Page404() {
     return (
         <div>
-
             <div className="page-wrapper">
                 <div className="circles-layer">
                     <div className="circle -white"></div>
@@ -29,7 +27,6 @@ function Page404() {
                     <div className="test">Opps... looks like You got lost</div><a className="button" href="/">Home</a>
                 </div>
             </div>
-
         </div>
     )
 }
