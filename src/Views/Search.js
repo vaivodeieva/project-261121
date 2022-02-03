824c7d47abc9f01ac974f1adcae9ef8f0c4b763e
vaivodeieva <ivaivode@inbox.lv>
2022-02-03 12:55:05 +0200

    Update Search.js

--- src/Views/Search.js
+++ src/Views/Search.js
@@ -1,17 +1,10 @@
 import '../Assets/Css/Search.css';
-
 function Search() {
     window.addEventListener("load", () => {
-        // (A) GET HTML ELEMENTS
-        var filter = document.getElementById("the-filter"), // search box
-          list = document.querySelectorAll("#the-list li"); // all list items
-      
-        // (B) ATTACH KEY UP LISTENER TO SEARCH BOX
-        filter.onkeyup = () => {
-          // (B1) GET CURRENT SEARCH TERM
+        var filter = document.getElementById("the-filter"), 
+          list = document.querySelectorAll("#the-list li"); 
+              filter.onkeyup = () => {
           let search = filter.value.toLowerCase();
-      
-          // (B2) LOOP THROUGH LIST ITEMS - ONLY SHOW THOSE THAT MATCH SEARCH
           for (let i of list) {
             let item = i.innerHTML.toLowerCase();
             if (item.indexOf(search) == -1) {
@@ -22,14 +15,11 @@ function Search() {
           }
         };
       });
-      
     return (
         <div className='search-container'>
             <div className="widget-wrap col-lg-6 md-4">
                 <h2 className='col-sm-2'>SEARCHABLE LIST</h2>
-
                 <input type="text" id="the-filter" placeholder="Search For..." autocomplete="off" />
-
                 <ul id="the-list">
                     <li>Apple</li>
                     <li>Apricots</li>
@@ -61,5 +51,4 @@ function Search() {
         </div>
     )
 }
-
 export default Search;
